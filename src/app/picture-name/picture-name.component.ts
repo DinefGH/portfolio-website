c0d0cb67415d0e82c0132280f5a7ccb683af8687
DinefGH <fabianduerr@web.de>
2023-08-15 10:27:19 +0200

    design fixes

--- src/app/picture-name/picture-name.component.ts
+++ src/app/picture-name/picture-name.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-picture-name',
   templateUrl: './picture-name.component.html',
   styleUrls: ['./picture-name.component.scss']
 })
-export class PictureNameComponent {
+export class PictureNameComponent implements OnInit {
+  textToDisplay = '';
 
+  private fullText = 'Fabian Dürr';
+
+  ngOnInit(): void {
+    this.animateText();
+  }
+
+  animateText(): void {
+    // Delay for half a second before starting the typing effect.
+    setTimeout(() => {
+      this.textToDisplay = this.fullText;
+    }, 500);
+  }
 }
